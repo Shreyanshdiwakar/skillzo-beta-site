c756fd796bd2a12ecb6eaa06ad70d1799bdbd18f
anay-208 <me@anayparaswani.dev>
2025-06-24 20:09:58 +0000

    some more changes

--- apps/next/src/components/footer.tsx
+++ apps/next/src/components/footer.tsx
@@ -62,22 +62,14 @@ export default function Footer() {
           </p>
           <div className="flex items-center gap-4">
             <Link href="https://instagram.com/skillzo.co" target="_blank" rel="noopener" aria-label="Instagram" className="hover:text-primary transition flex items-center justify-center">
-              <Image
-                src="/logos/instagram.png"
-                alt="Instagram"
-                width={32}
-                height={32}
-                className="object-contain"
-              />
+              <svg width="32" height="32" viewBox="0 0 448 512" fill="white" xmlns="http://www.w3.org/2000/svg">
+                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9 114.9-51.3 114.9-114.9S287.7 141 224.1 141zm0 186c-39.5 0-71.5-32-71.5-71.5s32-71.5 71.5-71.5 71.5 32 71.5 71.5-32 71.5-71.5 71.5zm146.4-194.3c0 14.9-12 26.9-26.9 26.9s-26.9-12-26.9-26.9 12-26.9 26.9-26.9 26.9 12 26.9 26.9zm76.1 27.2c-1.7-35.3-9.9-66.7-36.2-92.9S388.6 9.7 353.3 8c-35.3-1.7-141.3-1.7-176.6 0-35.3 1.7-66.7 9.9-92.9 36.2S9.7 123.4 8 158.7c-1.7 35.3-1.7 141.3 0 176.6 1.7 35.3 9.9 66.7 36.2 92.9s57.6 34.5 92.9 36.2c35.3 1.7 141.3 1.7 176.6 0 35.3-1.7 66.7-9.9 92.9-36.2s34.5-57.6 36.2-92.9c1.7-35.3 1.7-141.3 0-176.6zM398.8 388c-7.8 19.6-22.9 34.7-42.5 42.5-29.4 11.7-99.2 9-132.3 9s-102.9 2.6-132.3-9c-19.6-7.8-34.7-22.9-42.5-42.5-11.7-29.4-9-99.2-9-132.3s-2.6-102.9 9-132.3c7.8-19.6 22.9-34.7 42.5-42.5 29.4-11.7 99.2-9 132.3-9s102.9-2.6 132.3 9c19.6 7.8 34.7 22.9 42.5 42.5 11.7 29.4 9 99.2 9 132.3s2.6 102.9-9 132.3z"/>
+              </svg>
             </Link>
             <Link href="https://www.linkedin.com/company/skillzoco/" target="_blank" rel="noopener" aria-label="LinkedIn" className="hover:text-primary transition flex items-center justify-center">
-              <Image
-                src="/logos/linkedin.png"
-                alt="LinkedIn"
-                width={32}
-                height={32}
-                className="object-contain"
-              />
+              <svg width="32" height="32" viewBox="0 0 448 512" fill="white" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100.28 448H7.4V148.9h92.88zm-46.44-340.7C24.09 107.3 0 83.2 0 53.6A53.6 53.6 0 0 1 53.6 0c29.6 0 53.6 24.09 53.6 53.6 0 29.6-24.09 53.7-53.6 53.7zM447.8 448h-92.4V302.4c0-34.7-12.4-58.4-43.3-58.4-23.6 0-37.6 15.9-43.7 31.3-2.3 5.6-2.8 13.4-2.8 21.2V448h-92.4s1.2-241.1 0-266.1h92.4v37.7c12.3-19 34.3-46.1 83.5-46.1 60.9 0 106.7 39.7 106.7 125.2V448z"/>
+              </svg>
             </Link>
           </div>
         </div>
