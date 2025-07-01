b4b5be472a2ac66cf4925251d4b457a9e0c9d3b0
anay-208 <me@anayparaswani.dev>
2025-07-02 01:10:19 +0800

    temporarily remove db

--- apps/next/src/components/actions.ts
+++ apps/next/src/components/actions.ts
@@ -1,5 +1,5 @@
 "use server";
-import prisma from "@skillzo/db";
+// import prisma from "@skillzo/db";
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_SECRET_KEY);
@@ -12,12 +12,12 @@ export async function subscribeToNewsletter(email: string) {
     subject: "New Newsletter signup",
     text: `Dear Adarsh, \n We've noticed a new newsletter signup from ${email}. \n If you've any queries, please don't hesitate to contact Anay at me@anayparaswani.dev \n Thanks & Regards, \n Automated email from website`
   })
-  const existing = await prisma.newsletter.findUnique({ where: { email } });
-  if (existing) {
+  // const existing = await prisma.newsletter.findUnique({ where: { email } });
+  // if (existing) {
     // return { success: false, message: "You are already subscribed to the newsletter." };
-  }
-  if (!existing){
-    await prisma.newsletter.create({ data: { email } });
-  }
+  // }
+  // if (!existing){
+  //   await prisma.newsletter.create({ data: { email } });
+  // }
   return { success: true, message: "You have successfully subscribed!" };
 }
