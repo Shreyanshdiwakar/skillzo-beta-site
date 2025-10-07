f32c7b41490520f2e06e8b94aa734fe3cc65311e
Shreyanshdiwakar <shreyanshdiwakar18@gmail.com>
2025-10-07 18:52:46 +0530

    Optimize for Vercel deployment: Update build scripts, add image domains, create vercel.json

--- apps/next/next.config.ts
+++ apps/next/next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig, Redirect } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    // remotePatterns: [new URL("https://placehold.co/**")],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
     // dangerouslyAllowSVG: true
   },
 
